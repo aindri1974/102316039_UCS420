7c5742856b9b099b6cc28b072374859ab7372f7f
Aindri Singh <aindrisingh1974@gmail.com>
2025-01-20 12:03:19 +0530

    First Assignment

---
+++ Assignment 2.py
@@ -0,0 +1,34 @@
+##Question 1
+"""
+L = [10, 20, 30, 40, 50, 60, 70, 80]
+L.append(200)
+L.append(300)
+print("After adding 200 and 300:", L)
+L.remove(10)
+L.remove(30)
+print("After removing 10 and 30:", L)
+L.sort()
+print("Sorted in ascending order:", L)
+L.sort(reverse=True)
+print("Sorted in descending order:", L)
+"""
+
+##Question 2
+"""
+scores = (45, 89.5, 76, 45.4, 89, 92, 58, 45)
+highest_score = max(scores)
+highest_index = scores.index(highest_score)
+print("Highest score:", highest_score, "at index:", highest_index)
+lowest_score = min(scores)
+lowest_count = scores.count(lowest_score)
+print("Lowest score:", lowest_score, "occurs", lowest_count, "times")
+reversed_list = list(scores[::-1])
+print("Reversed tuple as a list:", reversed_list)
+score_to_check = 76  
+if score_to_check in scores:
+    print(f"Score {score_to_check} is present at index {scores.index(score_to_check)}")
+else:
+    print(f"Score {score_to_check} is not present")
+"""
+## Question 3
+
